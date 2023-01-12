35fde5384002e9a3bb44fd82dc089cc730c51189
Reyno Andriano <reyno@gmail.com>
2023-01-12 16:58:44 +0700

    Adding parameters

--- src/GcfValidateToken.php
+++ src/GcfValidateToken.php
@@ -4,9 +4,17 @@ namespace Devilian\GcfValidateToken;
 
 class GcfValidateToken
 {
+    public ?string $token;
+    public ?string $project;
+
+    public function __construct(string $token, ?string $project)
+    {
+        $this->token = $token;
+        $this->project = $project;
+    }
 
     public function validate()
     {
-        return 'hello packagist';
+        return $this->token . " " . $this->project;
     }
 }
