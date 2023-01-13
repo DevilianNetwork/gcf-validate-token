b51c0ea0dd96d4868526eee7a3373957e463f677
Reyno Andriano <reyno@gmail.com>
2023-01-13 08:28:14 +0700

    Ready to release

--- src/GcfValidateToken.php
+++ src/GcfValidateToken.php
@@ -1,6 +1,19 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Devilian\GcfValidateToken;
+
+use DateTimeImmutable;
+use DateTimeZone;
+use Lcobucci\JWT\Encoding\CannotDecodeContent;
+use Lcobucci\JWT\Encoding\JoseEncoder;
+use Lcobucci\JWT\Token\InvalidTokenStructure;
+use Lcobucci\JWT\Token\Parser;
+use Lcobucci\JWT\Token\UnsupportedHeaderFound;
+use Lcobucci\JWT\UnencryptedToken;
+
+require 'vendor/autoload.php';
 
 class GcfValidateToken
 {
@@ -15,6 +28,49 @@ class GcfValidateToken
 
     public function validate()
     {
-        return $this->token . " " . $this->project;
+        $parser = new Parser(new JoseEncoder());
+
+        try {
+            $tokenCheck = $parser->parse($this->token);
+        } catch (CannotDecodeContent | InvalidTokenStructure | UnsupportedHeaderFound $e) {
+            return [
+                'error' => $e->getMessage()
+            ];
+        }
+        assert($tokenCheck instanceof UnencryptedToken);
+
+        if ($tokenCheck->claims()->get('iss') !== 'https://securetoken.google.com/' . $this->project) {
+            return [
+                'error' => 'Invalid iss'
+            ];
+        }
+
+        if ($tokenCheck->claims()->get('aud')[0] !== $this->project) {
+            return [
+                'error' => 'Invalid aud'
+            ];
+        }
+
+        if (time() > $tokenCheck->claims()->get('exp')->getTimestamp()) {
+            return [
+                'error' => 'Expired token'
+            ];
+        }
+
+        return [
+            "auth_time" => $tokenCheck->claims()->get('auth_time'),
+            "user_id" => $tokenCheck->claims()->get('user_id'),
+            "email" => $tokenCheck->claims()->get('email'),
+            "name" => $tokenCheck->claims()->get('name'),
+            "email_verified" => $tokenCheck->claims()->get('email_verified'),
+            "iss" => $tokenCheck->claims()->get('iss'),
+            "aud" => $tokenCheck->claims()->get('aud'),
+            "sub" => $tokenCheck->claims()->get('sub'),
+            "iat" => $tokenCheck->claims()->get('iat'),
+            "exp" => $tokenCheck->claims()->get('exp'),
+            "validation_time" => new DateTimeImmutable("now", new DateTimeZone('+00:00')),
+            "token" => $this->token,
+            "project" => $this->project
+        ];
     }
 }
